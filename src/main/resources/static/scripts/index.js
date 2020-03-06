a8649441e6782cec7347353afa667c7a02562285
Ryan.Wang <ranwang1995@outlook.com>
2020-03-06 11:47:13 +0800

    [WangRan] feat: finish basic page layout

---
+++ src/main/resources/static/scripts/index.js
@@ -0,0 +1,11 @@
+let usernameEl = document.getElementsByClassName("username-input")[0];
+let passwordEl = document.getElementsByClassName("password-input")[0];
+let usernameInput;
+let passwordInput;
+
+function onSubmitClick() {
+    usernameInput = usernameEl.value;
+    passwordInput = passwordEl.value;
+
+
+}
