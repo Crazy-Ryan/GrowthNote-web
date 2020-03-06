c02c2da14cb5ba2a94318727391155c11e5cd6c4
Ryan.Wang <ranwang1995@outlook.com>
2020-03-06 12:11:41 +0800

    [WangRan] feat: add method to validate username and password input

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -2,10 +2,34 @@ let usernameEl = document.getElementsByClassName("username-input")[0];
 let passwordEl = document.getElementsByClassName("password-input")[0];
 let usernameInput;
 let passwordInput;
+let inputCorrect = false;
 
 function onSubmitClick() {
     usernameInput = usernameEl.value;
     passwordInput = passwordEl.value;
+    validateUserCredential(usernameInput,passwordInput);
+}
+
+function validateUserCredential(username, password) {
+
+    retrieveCredentialInfo(username, password);
+    function retrieveCredentialInfo(username, password) {
+        let getAJAXJsonOption = {
+            url: 'http://localhost:8080/api/users?name=' + username + '&password=' + password,
+            method: 'GET',
+            success: function (responseText) {
+                console.log(responseText);
+                inputCorrect = isCorrect(responseText);
+            },
+            fail: function (error) {
+                console.log('get data error');
+            }
+        };
+        ajaxJsonHandle(getAJAXJsonOption);
+    }
 
 }
 
+function isCorrect(userInfo) {
+    return null != userInfo.id;
+}
