26f36c809bb47bac411e5bab2c8357c8169c1eb2
Ryan.Wang <ranwang1995@outlook.com>
2020-03-06 14:42:30 +0800

    [WangRan] feat: finish register page

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,33 +1,26 @@
-let usernameEl = document.getElementsByClassName("username-input")[0];
-let passwordEl = document.getElementsByClassName("password-input")[0];
-let usernameInput;
-let passwordInput;
 let inputCorrect = false;
 
 function onSubmitClick() {
-    usernameInput = usernameEl.value;
-    passwordInput = passwordEl.value;
-    validateUserCredential(usernameInput,passwordInput);
+    let usernameInput = document.getElementsByClassName("username-input")[0].value;
+    let passwordInput = document.getElementsByClassName("password-input")[0].value;
+    validateUserCredential(usernameInput, passwordInput);
 }
 
 function validateUserCredential(username, password) {
-
-    retrieveCredentialInfo(username, password);
-    function retrieveCredentialInfo(username, password) {
-        let getAJAXJsonOption = {
-            url: 'http://localhost:8080/api/users?name=' + username + '&password=' + password,
-            method: 'GET',
-            success: function (responseText) {
-                console.log(responseText);
-                inputCorrect = isCorrect(responseText);
-            },
-            fail: function (error) {
-                console.log('get data error');
-            }
-        };
-        ajaxJsonHandle(getAJAXJsonOption);
-    }
-
+    console.log(username);
+    console.log(password);
+    let getAJAXJsonOption = {
+        url: 'http://localhost:8080/api/users?name=' + username + '&password=' + password,
+        method: 'GET',
+        success: function (responseText) {
+            console.log(responseText);
+            inputCorrect = isCorrect(responseText);
+        },
+        fail: function (error) {
+            console.log('get data error');
+        }
+    };
+    ajaxJsonHandle(getAJAXJsonOption);
 }
 
 function isCorrect(userInfo) {
---
+++ src/main/resources/static/scripts/register.js
@@ -0,0 +1,24 @@
+function onRegisterClick() {
+    let usernameInput = document.getElementsByClassName("username-input")[0].value;
+    let passwordInput = document.getElementsByClassName("password-input")[0].value;
+    registerWithCredential(usernameInput, passwordInput);
+}
+
+function registerWithCredential(username, password) {
+
+    let getAJAXJsonOption = {
+        url: 'http://localhost:8080/api/users',
+        method: 'POST',
+        data: {
+            "name": username,
+            "password": password
+        },
+        success: function (responseText) {
+            console.log(responseText);
+        },
+        fail: function (error) {
+            console.log('get data error');
+        }
+    };
+    ajaxJsonHandle(getAJAXJsonOption);
+}
